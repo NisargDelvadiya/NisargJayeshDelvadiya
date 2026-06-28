f6e5a1b173db4b7a7177148f9459c7badf961b20
Nisarg Delvadiya <nisarg.delvadiya1@gmail.com>
2026-06-28 14:43:55 +0530

    chore: Revert BigRock export configurations for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactCompiler: true,
   // output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
